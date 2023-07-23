eb029b7b1dc68f953b8bff92ecd697c377af0223
Pramit Bhatia <pramitbhatia25@gmail.com>
2023-07-23 17:00:14 -0400

    Edits

---
+++ streamlit_app.py
@@ -0,0 +1,278 @@
+import streamlit as st
+from utils import NASA_APIConnection
+from datetime import date as date_function
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+# Assuming you have already obtained your NASA API key
+NASA_API_KEY = os.getenv('NASA_API_KEY')
+
+# Create the NASA API connection
+nasa_conn = st.experimental_connection("nasa", type=NASA_APIConnection, api_key=NASA_API_KEY)
+
+# Streamlit app
+def main():
+    st.title("🚀NASA API Connection with Streamlit")
+    st.sidebar.info(
+    """
+    This is my submission to the Streamlit Connections Hackathon.
+    [Hackathon Link](https://discuss.streamlit.io/t/connections-hackathon/47574)
+    This app demonstrates a connection to query from NASA Open APIs.
+    Thanks for stopping by!
+    """
+    )
+
+    st.sidebar.info(
+        """
+        The application currently supports the following API's:
+        APOD, Mars Rover Photos, NeoWs, DONKI & Exoplanet API's
+        """
+    )
+    # Sidebar menu to select NASA API
+    selected_api = st.sidebar.selectbox("Select a NASA API", ["APOD", "Near-Earth Object Web Service (NEOWS)", "Mars Rover Photos", "DONKI: Space Weather Database", "Exoplanet"])
+
+    if selected_api == "APOD":
+        today = date_function.today()
+        d1 = today.strftime("%b %d, %Y")
+
+
+        st.subheader("Astronomy Picture of the Day (APOD)")
+        st.markdown(
+            """
+            **Astronomy Picture of the Day (APOD)**
+            The Astronomy Picture of the Day (APOD) is a website provided by NASA that features
+            a new and different image or photograph of the universe every day, along with a brief
+            explanation written by a professional astronomer.
+            """
+        )
+
+        st.write('**Date must be between Jun 16, 1995 and {}.**'.format(d1))
+        date = st.text_input("Enter a date in the format 'YYYY-MM-DD' or 'latest' for the latest APOD:")
+ 
+        if st.button("Get APOD"):
+            if date:
+                apod_data = nasa_conn.query_apod(date)
+
+                if apod_data is not None:
+                    st.write(f"Date: {apod_data['date'].values[0]}")
+                    st.write(f"Title: {apod_data['title'].values[0]}")
+                    st.image(apod_data['url'].values[0], caption=apod_data['title'].values[0])
+                    st.write(f"Explanation: {apod_data['explanation'].values[0]}")
+                else:
+                    st.error("Failed to fetch APOD data")
+
+    elif selected_api == "Mars Rover Photos":
+        st.subheader("Mars Rover Photos")
+        st.markdown(
+            """
+            **Mars Rover Photos**
+            The Mars Rover Photos API allows you to explore the photos taken by NASA's rovers on Mars.
+            You can select a rover (Curiosity, Opportunity, or Spirit) and enter a Martian sol (a Martian day)
+            to view the corresponding photos captured by the selected rover on that sol.
+
+            Select a rover from the dropdown menu, enter a Martian sol, and click the 'Get Photos' button
+            to view the images taken on the specified sol.
+
+            Only the top 100 photos are retrieved.
+            """
+        )
+        rover_name = st.selectbox("Select a rover", ["Curiosity", "Opportunity", "Spirit"])
+        sol = st.text_input("Enter a Martian sol (e.g., 1000):")
+
+        if st.button("Get Photos"):
+            if sol:
+                mars_rover_photos = nasa_conn.query_mars_rover_photos(rover_name, sol)
+
+                if mars_rover_photos is not None:
+                    st.write(f"Rover: {rover_name}")
+                    st.write(f"Number of Photos: 100")
+
+                    st.write(mars_rover_photos)
+                    df = mars_rover_photos
+                    num_images = len(df)
+                    rows = (num_images // 3) + 1
+
+                    for i in range(rows):
+                        cols = min(3, num_images - i * 3)
+
+                        for j in range(cols):
+                            idx = i * 3 + j
+                            img_url = df.iloc[idx]["img_src"]
+                            st.image(img_url, caption=f"Earth Date: {df.iloc[idx]['earth_date']}", width=300)
+
+                else:
+                    st.error("Failed to fetch Mars rover photos")
+    elif selected_api == "Near-Earth Object Web Service (NEOWS)":
+
+        st.subheader("Near-Earth Object Web Service (NEOWS)")
+        st.markdown(
+            """
+            **Near-Earth Object Web Service (NEOWS)**
+            The Near-Earth Object Web Service (NEOWS) API provides information about near-Earth asteroids and comets.
+            You can use this API to get a list of close-approaching objects for a given date range or lookup specific
+            objects by their IDs.
+
+            Enter a date range (start_date and end_date) and click the 'Get Close-Approaching Objects' button to view
+            a list of near-Earth objects for the specified date range.
+            """
+        )
+        st.write('**Date range must be within 7 days.**')
+        st.write('Eg. startDate=2022-07-01, endDate= 2022-07-08 is valid.')
+        st.write('Eg. startDate=2022-07-01, endDate= 2022-07-09 is invalid.')
+        start_date = st.text_input("Enter the start date in the format 'YYYY-MM-DD':")
+        end_date = st.text_input("Enter the end date in the format 'YYYY-MM-DD':")
+
+        if st.button("Get Close-Approaching Objects"):
+            if start_date and end_date:
+                neows_data = nasa_conn.query_neows(start_date, end_date)
+
+                if neows_data is not None:
+                    st.write(f"Start Date: {start_date}")
+                    st.write(f"End Date: {end_date}")
+
+                    st.write(neows_data)
+                else:
+                    st.error("Failed to fetch NEOWS data")                    
+    elif selected_api == "DONKI: Space Weather Database":
+        st.subheader("NASA DONKI Database Query")
+        st.markdown(
+            """
+            **DONKI: Space Weather Database**
+            This app lets you query the NASA DONKI (Space Weather Database Of Notifications, Knowledge, Information) 
+            to find various space weather events and related information. You can search for events based on different 
+            parameters. Happy space weather exploration!
+            """
+        )
+
+        st.write("**Please allow the date range gap to be atleast 30 days.**")
+        st.write("**Please limit the date range gap to be a maximum of 60 days for fastest API CALL Times.**")
+
+        # Event types supported by DONKI
+        donki_event_types = [
+            "Coronal Mass Ejection (CME)",
+            "Geomagnetic Storm (GST)",
+            "Solar Flare (FLR)",
+            "Solar Energetic Particle (SEP)",
+            "Magnetopause Crossing (MPC)",
+            "Radiation Belt Enhancement (RBE)",
+            "High-Speed Stream (HSS)",
+            "Notifications"
+        ]
+
+        # Sidebar menu to select DONKI event type
+        selected_donki_event = st.selectbox("Select a DONKI Event Type", donki_event_types)
+
+        if selected_donki_event == "Coronal Mass Ejection (CME)":
+            start_date = st.text_input("Enter the start date in the format 'YYYY-MM-DD':")
+            end_date = st.text_input("Enter the end date in the format 'YYYY-MM-DD':")
+
+            if st.button("Get CME Data"):
+                if start_date and end_date:
+                    cme_data = nasa_conn.query_donki(start_date, end_date, type="CME")
+                    st.write(cme_data)
+
+        elif selected_donki_event == "Geomagnetic Storm (GST)":
+            start_date = st.text_input("Enter the start date in the format 'YYYY-MM-DD':")
+            end_date = st.text_input("Enter the end date in the format 'YYYY-MM-DD':")
+
+            if st.button("Get GST Data"):
+                if start_date and end_date:
+                    gst_data = nasa_conn.query_donki(start_date, end_date, type="GST")
+                    st.write(gst_data)
+
+        elif selected_donki_event == "Solar Flare (FLR)":
+            start_date = st.text_input("Enter the start date in the format 'YYYY-MM-DD':")
+            end_date = st.text_input("Enter the end date in the format 'YYYY-MM-DD':")
+
+            if st.button("Get FLR Data"):
+                if start_date and end_date:
+                    flr_data = nasa_conn.query_donki(start_date, end_date, type="FLR")
+                    st.write(flr_data)
+
+        elif selected_donki_event == "Solar Energetic Particle (SEP)":
+            start_date = st.text_input("Enter the start date in the format 'YYYY-MM-DD':")
+            end_date = st.text_input("Enter the end date in the format 'YYYY-MM-DD':")
+
+            if st.button("Get SEP Data"):
+                if start_date and end_date:
+                    sep_data = nasa_conn.query_donki(start_date, end_date, type="SEP")
+                    st.write(sep_data)
+
+        elif selected_donki_event == "Magnetopause Crossing (MPC)":
+            start_date = st.text_input("Enter the start date in the format 'YYYY-MM-DD':")
+            end_date = st.text_input("Enter the end date in the format 'YYYY-MM-DD':")
+
+            if st.button("Get MPC Data"):
+                if start_date and end_date:
+                    mpc_data = nasa_conn.query_donki(start_date, end_date, type="MPC")
+                    st.write(mpc_data)
+
+        elif selected_donki_event == "Radiation Belt Enhancement (RBE)":
+            start_date = st.text_input("Enter the start date in the format 'YYYY-MM-DD':")
+            end_date = st.text_input("Enter the end date in the format 'YYYY-MM-DD':")
+
+            if st.button("Get RBE Data"):
+                if start_date and end_date:
+                    rbe_data = nasa_conn.query_donki(start_date, end_date, type="RBE")
+                    st.write(rbe_data)
+
+        elif selected_donki_event == "High-Speed Stream (HSS)":
+            start_date = st.text_input("Enter the start date in the format 'YYYY-MM-DD':")
+            end_date = st.text_input("Enter the end date in the format 'YYYY-MM-DD':")
+
+            if st.button("Get HSS Data"):
+                if start_date and end_date:
+                    hss_data = nasa_conn.query_donki(start_date, end_date, type="HSS")
+                    st.write(hss_data)
+
+        elif selected_donki_event == "Notifications":
+            start_date = st.text_input("Enter the start date in the format 'YYYY-MM-DD':")
+            end_date = st.text_input("Enter the end date in the format 'YYYY-MM-DD':")
+
+            if st.button("Get Notifications Data"):
+                if start_date and end_date:
+                    not_data = nasa_conn.query_donki(start_date, end_date, type="notifications")
+                    st.write(not_data)
+
+    elif selected_api == "Exoplanet":
+        st.subheader("NASA Exoplanet Archive Query")
+        st.markdown(
+            """
+            **Exoplanet Archive**
+            The Exoplanet Archive API allows programmatic access to NASA's Exoplanet Archive database.
+            Enter the query to retrieve data from the database of all confirmed planets (and hosts) in the archive with parameters derived from a single, published reference that are designated as the archive's default parameter set.
+            Happy planet hunting!
+            """
+        )
+
+        # Examples to add custom queries for Exoplanet Archive
+        st.write("Example 1: Query the Kepler Objects of Interest (KOI) Cumulative Table for exoplanets with a Kepler disposition of 'CANDIDATE', an orbital period (koi_period) more than 300 days, and a planet radius (koi_prad) less than 2.")
+        exo_where_example1 = "koi_disposition like 'CANDIDATE' and koi_period > 300 and koi_prad < 2"
+        st.write("Query:")
+        st.write("select *  from cumulative where koi_disposition like 'CANDIDATE' and koi_period > 300 and koi_prad < 2")
+        if st.button("Get Exoplanet Data - Example 1"):
+            exo_data_example1 = nasa_conn.query_exoplanet_data(table="cumulative", where=exo_where_example1)
+            st.write(exo_data_example1)
+
+        # Custom query input fields
+        st.subheader("Custom Query")
+        select = st.text_input("Enter the 'select' clause to specify columns to return (optional):", value ="*")
+        where = st.text_input("Enter the 'where' clause to specify filtering conditions (optional):", value ="koi_disposition like 'CANDIDATE' and koi_period > 300 and koi_prad < 2")
+        order = st.text_input("Enter the 'order' clause to specify the order of rows (optional):", value ="kepid")
+
+        if st.button("Run Query"):
+            exoplanet_data = nasa_conn.query_exoplanet_data(
+                table="cumulative",
+                where=where,
+                select=select,
+                order=order,
+            )
+
+            if exoplanet_data is not None:
+                st.write(exoplanet_data)
+            else:
+                st.error("Failed to fetch Exoplanet Archive data")
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,246 @@
+import re
+from io import StringIO
+from typing import Any, Generator, Tuple, Optional, List
+import requests
+from requests.adapters import HTTPAdapter
+from streamlit.connections import ExperimentalBaseConnection
+from streamlit.runtime.caching import cache_data
+import pandas as pd
+from urllib3 import Retry
+
+class NASA_APIConnection(ExperimentalBaseConnection[requests.Session]):
+    """Basic st.experimental_connection implementation for NASA API"""
+
+    def __init__(self, connection_name: str,
+                 api_key: str,
+                 base_url: str = 'https://api.nasa.gov/',
+                 total_retries: int = 5,
+                 backoff_factor: float = 0.25,
+                 status_forcelist: List[int] = None,
+                 **kwargs):
+
+        self.api_key = api_key
+        self.base_url = base_url
+
+        if status_forcelist is None:
+            status_forcelist = [500, 502, 503, 504]
+
+        self.retries = Retry(total=total_retries, backoff_factor=backoff_factor, status_forcelist=status_forcelist)
+
+        super().__init__(connection_name, **kwargs)
+
+    def _connect(self, **kwargs: Any) -> requests.Session:
+        """Connects to the Session
+
+        :returns: requests.Session
+        """
+        session = requests.Session()
+        session.mount("https://", HTTPAdapter(max_retries=self.retries))
+        return session
+
+    def query_apod(self, date: str, cache_time: int = 3600, **kwargs: Any) -> pd.DataFrame:
+        """Queries the Astronomy Picture Of The Day API and returns a DataFrame.
+
+        :param date: specific date in the format 'YYYY-MM-DD' or 'latest' for the latest APOD
+        :param cache_time: time to cache the result
+        :param kwargs: other optional parameters
+        :returns: result as a DataFrame
+        """
+
+        @cache_data(ttl=cache_time)
+        def _query_apod(date: str, **kwargs: Any) -> pd.DataFrame:
+            params = {'api_key': self.api_key, **kwargs}
+            
+            if date.lower() == 'latest':
+                url = self.base_url + 'planetary/apod'
+            else:
+                url = self.base_url + 'planetary/apod'
+                params = {'date': date, 'api_key': self.api_key, **kwargs}
+
+            try:
+                response = self._instance.get(url, params=params)
+                response.raise_for_status()
+                data = response.json()
+
+                # Create a DataFrame from the JSON response
+                result = pd.DataFrame(data, index=[0])
+
+                return result
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                return None
+
+        return _query_apod(date, **kwargs)
+
+
+    def query_neows(self, start_date: str, end_date: str, cache_time: int = 3600, **kwargs: Any) -> pd.DataFrame:
+        """Queries the Near-Earth Object Web Service (NEOWS) API and returns a DataFrame.
+
+        :param start_date: start date in the format 'YYYY-MM-DD'
+        :param end_date: end date in the format 'YYYY-MM-DD'
+        :param cache_time: time to cache the result
+        :param kwargs: other optional parameters
+        :returns: result as a DataFrame
+        """
+
+        @cache_data(ttl=cache_time)
+        def _query_neows(start_date: str, end_date: str, **kwargs: Any) -> pd.DataFrame:
+            params = {'start_date': start_date, 'end_date': end_date, 'api_key': self.api_key, **kwargs}
+
+            url = self.base_url + 'neo/rest/v1/feed'
+
+            try:
+                response = self._instance.get(url, params=params)
+                response.raise_for_status()
+                data = response.json()
+
+                # Flatten the nested JSON response to create a DataFrame
+                neows_data = data['near_earth_objects']
+                neows_list = []
+                for date in data['near_earth_objects']:
+                    for obj in data['near_earth_objects'][date]:
+                        obj_data = {
+                            'id': obj['id'],
+                            'name': obj['name'],
+                            'neo_reference_id': obj['neo_reference_id'],
+                            'close_approach_date': obj['close_approach_data'][0]['close_approach_date'],
+                            'nasa_jpl_url': obj['nasa_jpl_url'],
+                            'absolute_magnitude_h': obj['absolute_magnitude_h'],
+                            'estimated_diameter_min_km': obj['estimated_diameter']['kilometers']['estimated_diameter_min'],
+                            'estimated_diameter_max_km': obj['estimated_diameter']['kilometers']['estimated_diameter_max'],
+                            'estimated_diameter_min_m': obj['estimated_diameter']['meters']['estimated_diameter_min'],
+                            'estimated_diameter_max_m': obj['estimated_diameter']['meters']['estimated_diameter_max'],
+                            'is_potentially_hazardous_asteroid': obj['is_potentially_hazardous_asteroid'],
+                            'relative_velocity_kms': obj['close_approach_data'][0]['relative_velocity']['kilometers_per_second'],
+                            'miss_distance_km': obj['close_approach_data'][0]['miss_distance']['kilometers'],
+                            'orbiting_body': obj['close_approach_data'][0]['orbiting_body']
+                        }
+
+                        neows_list.append(obj_data)
+
+                result = pd.DataFrame(neows_list)
+
+                return result
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                return None
+
+        return _query_neows(start_date, end_date, **kwargs)
+
+
+    def query_mars_rover_photos(self, rover_name: str, sol: str, cache_time: int = 3600, **kwargs: Any) -> pd.DataFrame:
+        """Queries the Mars Rover Photos API and returns a DataFrame containing photos for the specified rover and sol.
+
+        :param rover_name: name of the rover (Curiosity, Opportunity, or Spirit)
+        :param sol: Martian sol (a Martian day) to get photos for
+        :param cache_time: time to cache the result
+        :param kwargs: other optional parameters
+        :returns: result as a DataFrame
+        """
+
+        @cache_data(ttl=cache_time)
+        def _query_mars_rover_photos(rover_name: str, sol: str, **kwargs: Any) -> pd.DataFrame:
+            params = {'sol': sol, 'api_key': self.api_key, **kwargs}
+
+            url = self.base_url + f'mars-photos/api/v1/rovers/{rover_name}/photos'
+
+            try:
+                response = self._instance.get(url, params=params)
+                response.raise_for_status()
+                data = response.json()
+
+                # Extract photos from the JSON response
+                photos = data.get('photos', [])
+
+                # Create a DataFrame from the list of photos
+                result = pd.DataFrame(photos[0:100])
+
+                return result
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                return None
+
+        return _query_mars_rover_photos(rover_name, sol, **kwargs)
+
+    def query_donki(self, start_date: str = None, end_date: str = None, type: str = "all", cache_time: int = 3600, **kwargs: Any) -> pd.DataFrame:
+        """Queries the NASA DONKI API and returns a DataFrame containing space weather events.
+
+        :param start_date: start date in the format 'YYYY-MM-DD' (default: 30 days prior to current UTC date)
+        :param end_date: end date in the format 'YYYY-MM-DD' (default: current UTC date)
+        :param type: type of event (default is 'all')
+        :param cache_time: time to cache the result
+        :param kwargs: other optional parameters
+        :returns: result as a DataFrame
+        """
+
+        @cache_data(ttl=cache_time)
+        def _query_donki(start_date: str, end_date: str, type: str, **kwargs: Any) -> pd.DataFrame:
+            params = {
+                'startDate': start_date,
+                'endDate': end_date,
+                'api_key': self.api_key,
+                **kwargs
+            }
+
+            url = self.base_url + f'DONKI/{type}'
+
+            try:
+                response = self._instance.get(url, params=params)
+                response.raise_for_status()
+                data = response.json()
+                
+                # Create a DataFrame from the JSON response
+                try:
+                    result = pd.DataFrame(data)
+                except:
+                    result = data
+
+                return result
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                return None
+
+        return _query_donki(start_date, end_date, type, **kwargs)
+    def query_exoplanet_data(self, table: str, where: str = None, select: str = None, order: str = None, format: str = "csv", cache_time: int = 3600, **kwargs: Any) -> pd.DataFrame:
+        """Queries the NASA Exoplanet Archive API and returns a DataFrame containing exoplanet data.
+
+        :param table: name of the data table to query (e.g., 'cumulative')
+        :param where: 'where' clause to specify filtering conditions (optional)
+        :param select: 'select' clause to specify columns to return (optional)
+        :param order: 'order' clause to specify the order of rows (optional)
+        :param format: preferred output file format ('csv' or 'ipac') (default: 'csv')
+        :param cache_time: time to cache the result
+        :param kwargs: other optional parameters
+        :returns: result as a DataFrame
+        """
+
+        @cache_data(ttl=cache_time)
+        def _query_exoplanet_data(table: str, where: str, select: str, order: str, **kwargs: Any) -> pd.DataFrame:
+            base_url = "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?"
+            params = {
+                'select': select,
+                'order': order,
+                'api_key': self.api_key,
+                **kwargs
+            }
+
+            # Construct the query URL with table and other parameters
+            url = base_url + f'table={table}'
+            if where:
+                url += f'&where={where}'
+
+            try:
+                response = self._instance.get(url, params=params)
+                response.raise_for_status()
+                data = response.text
+
+                # Handle different output formats (csv or ipac)
+                if format == "csv":
+                    result = pd.read_csv(StringIO(data))
+
+                return result
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                return None
+
+        return _query_exoplanet_data(table, where, select, order, format, **kwargs)
